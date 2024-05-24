7ac34b51a26d0fcade7821953e68b89aad808c3f
Joshua Bartholomew <joshua.bartholomew@realtruck.com>
2024-05-24 03:49:38 +0000

    Move initial ls to standalone function

--- bandit0.py
+++ bandit0.py
@@ -6,18 +6,20 @@ bandit_port = 2220
 level_username = "bandit0"
 level_password = "bandit0"
 
-if __name__ == "__main__":
-    connection = ssh(level_username, bandit_host, bandit_port, level_password)
-    shell = connection.shell("/usr/bin/bash")
+def basic_reconnaissance(remote_connection):
+    shell = remote_connection.shell()
     shell.sendline(b"ls -l")
     shell.sendline(b"exit")
     shell_output = shell.recvall()
+    print("Initial login and filesystem info:\n\n")
+    print(shell_output.decode())
+
+if __name__ == "__main__":
+    connection = ssh(level_username, bandit_host, bandit_port, level_password)
+    if connection.connected():
+        basic_reconnaissance(connection)
     connection.close()
 
-    printable_output = repr(shell_output)
-    print(printable_output)
-
-    
     # next_level_password = shell.readline().decode()
     # print(f"The password for the next level is: {next_level_password}")
 
