9c3496fec9ae4f906f996510ad41ae15855a78c1
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2024-09-20 22:09:19 -0500

    Add function to read password

--- bandit0.py
+++ bandit0.py
@@ -10,12 +10,24 @@ def basic_reconnaissance(remote_connection):
     shell_output = remote_connection.ls("-l")
     print(shell_output.decode())
 
+def collect_password(remote_connection):
+    flag = False
+    password = ""
+    shell_output = remote_connection.cat("readme")
+    formatted_output = shell_output.decode()
+    for char in formatted_output:
+        if char == ":":
+            flag = True
+        if char != " " and flag == True:
+            password += char
+    return password
+
 if __name__ == "__main__":
     connection = ssh(username, host, port, password)
     if connection.connected():
         basic_reconnaissance(connection)
+        next_level_password = collect_password(connection)
     connection.close()
 
-    # next_level_password = shell.readline().decode()
-    # print(f"The password for the next level is: {next_level_password}")
+    print(f"The password for the next level is: {next_level_password}")
 
